c74b12aa520815451d80cc9bbcadfdae21e89834
AustinPhillipTaylor <47870619+AustinPhillipTaylor@users.noreply.github.com>
2022-03-24 23:52:01 -0400

    Moved logic to settings service

--- api/src/controllers/settings.ts
+++ api/src/controllers/settings.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import { ForbiddenException, InvalidPayloadException } from '../exceptions';
+import { ForbiddenException } from '../exceptions';
 import { respond } from '../middleware/respond';
 import useCollection from '../middleware/use-collection';
 import { SettingsService } from '../services';
 import asyncHandler from '../utils/async-handler';
-import { isValidThemeJson } from '../utils/validate-theme-json';
-import { omitBy, isEmpty } from 'lodash';
 
 const router = express.Router();
 
@@ -38,46 +36,12 @@ router.get(
 router.patch(
 	'/themes',
 	asyncHandler(async (req, res, next) => {
-		const error = isValidThemeJson(req.body);
-		if (error !== undefined) {
-			throw new InvalidPayloadException(error.message);
-		}
-
 		const service = new SettingsService({
 			accountability: req.accountability,
 			schema: req.schema,
 		});
 
-		let combinedOverrides = {};
-
-		/**
-		 * We only want to overwrite a theme's overrides if it's explicitly passed.
-		 * So here we'll get the current overrides then combine them with the
-		 * overrides from the request body.
-		 */
-		try {
-			const record = await service.readSingleton(req.sanitizedQuery);
-			const currentOverrides = record.theme_overrides || {};
-			/**
-			 * We'll allow deleting theme overrides by passing an empty object.
-			 * We'll omit any empty objects before upserting the value.
-			 */
-			combinedOverrides = omitBy({ ...currentOverrides, ...req.body }, isEmpty);
-		} catch (error: any) {
-			if (error instanceof ForbiddenException) {
-				return next();
-			}
-
-			throw error;
-		}
-
-		/**
-		 * This endpoint only accepts the theme list, we need to modify
-		 * the query to target the theme_overrides column
-		 */
-		const column = { theme_overrides: combinedOverrides };
-
-		await service.upsertSingleton(column);
+		await service.updateThemeOverrides(req.body, req.sanitizedQuery);
 
 		try {
 			const record = await service.readSingleton(req.sanitizedQuery);
@@ -98,15 +62,6 @@ router.patch(
 router.patch(
 	'/',
 	asyncHandler(async (req, res, next) => {
-		/**
-		 * We want to have more modular control over how themes are saved
-		 * so we'll enforce usage of the /themes endpoint instead of directly
-		 * altering theme_overrides
-		 */
-		if (req.body.theme_overrides !== undefined) {
-			throw new InvalidPayloadException('Themes should be updated at the /settings/themes endpoints.');
-		}
-
 		const service = new SettingsService({
 			accountability: req.accountability,
 			schema: req.schema,
