1dc726a3e17c9fa8d64ea61f21d44d4c0c0b2b03
AustinPhillipTaylor <47870619+AustinPhillipTaylor@users.noreply.github.com>
2022-04-21 12:20:06 -0400

    Added text block interface

---
+++ app/src/interfaces/presentation-text-block/presentation-text-block.vue
@@ -0,0 +1,21 @@
+<template>
+	<p class="presentation-text-block">
+		{{ text }}
+	</p>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+	text?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	text: '',
+});
+</script>
+
+<style lang="scss" scoped>
+.presentation-text-block {
+	color: var(--g-color-foreground-subtle);
+}
+</style>
